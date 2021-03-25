f2462f6c6c487b12b62027d1a7f7268e66275502
Judicaelle6 <judicaelle.six@gmail.com>
2021-03-25 19:41:02 +0100

    new changes

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import ExerciceFourView from './ex4/index';
+import { ExerciseFourView } from './ex4/index';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ExerciceFourView />
+        <ExerciseFourView />
       </header>
     </div>
   );
